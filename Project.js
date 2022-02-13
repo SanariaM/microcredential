b9d9a88795e0a248680a0c37601118c7a5f51182
SanariaM <83135464+SanariaM@users.noreply.github.com>
2022-02-13 12:44:41 -0500

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,16 @@
+var toggler = document.getElementsByClassName("title2");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("check-title2");
+  });
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
